4ae40695d9637cd9fa9ecb3bf67b3332d29c9abb
daianna21 <daianna2121@outlook.es>
2022-02-02 12:31:46 -0600

    Add R script of recount3

---
+++ R/08_recount3.R
@@ -0,0 +1,28 @@
+## Load recount3 R package
+library("recount3")
+## Revisemos todos los proyectos con datos de humano en recount3
+human_projects <- available_projects()
+## Encuentra tu proyecto de interés. Aquí usaremos
+## SRP009615 de ejemplo
+proj_info <- subset(
+  human_projects,
+  project == "SRP009615" & project_type == "data_sources"
+)
+## Crea un objetio de tipo RangedSummarizedExperiment (RSE)
+## con la información a nivel de genes
+rse_gene_SRP009615 <- create_rse(proj_info)
+
+## Explora el objeto RSE
+rse_gene_SRP009615
+
+## Convirtamos las cuentas por nucleotido a cuentas por lectura
+## usando compute_read_counts().
+assay(rse_gene_SRP009615, "counts") <- compute_read_counts(rse_gene_SRP009615)
+
+## Para este estudio en específico, hagamos más fácil de usar la
+## información del experimento
+rse_gene_SRP009615 <- expand_sra_attributes(rse_gene_SRP009615)
+colData(rse_gene_SRP009615)[
+  ,
+  grepl("^sra_attribute", colnames(colData(rse_gene_SRP009615)))
+  ]
