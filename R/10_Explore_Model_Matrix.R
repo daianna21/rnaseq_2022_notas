b1c324eff2588110a4490a305f401d4b83cac748
daianna21 <daianna2121@outlook.es>
2022-02-05 00:01:47 -0600

    Add Explore model matrix script

---
+++ R/10_Explore_Model_Matrix.R
@@ -0,0 +1,14 @@
+## Datos de ejemplo
+(sampleData <- data.frame(
+  genotype = rep(c("A", "B"), each = 4),
+  treatment = rep(c("ctrl", "trt"), 4)
+))
+
+## Creemos las imágenes usando ExploreModelMatrix
+vd <- ExploreModelMatrix::VisualizeDesign(
+  sampleData = sampleData,
+  designFormula = ~ genotype + treatment,
+  textSizeFitted = 4
+)
+## Veamos las imágenes
+cowplot::plot_grid(plotlist = vd$plotlist)
